d9f2a6534f92bdeee4c5d5455b959fb58d25a383
Henry Segal <henrysegal@gmail.com>
2023-02-26 20:30:09 -0500

    Add keychron q2 segal configuration

---
+++ keyboards/keychron/q2/ansi_encoder/keymaps/segal/rgb_matrix_user.c
@@ -0,0 +1,17 @@
+#include QMK_KEYBOARD_H
+
+void keyboard_post_init_user(void) {
+    rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
+    rgb_matrix_sethsv_noeeprom(HSV_OFF);
+}
+
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    if (host_keyboard_led_state().caps_lock) {
+        for (uint8_t i = led_min; i < led_max; i++) {
+            if (g_led_config.flags[i] & LED_FLAG_KEYLIGHT) {
+                rgb_matrix_set_color(i, RGB_WHITE);
+            }
+        }
+    }
+    return false;
+}
---
+++ keyboards/keychron/q2/ansi_encoder/keymaps/segal/rules.mk
@@ -0,0 +1,4 @@
+VIA_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
+
+SRC += rgb_matrix_user.c
